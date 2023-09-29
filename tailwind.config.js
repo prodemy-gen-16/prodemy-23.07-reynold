69728e16c151837ac5599bae997ecf39a427367c
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-09-29 14:03:47 +0700

    update content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./gen-23.07-tailwind-css/src/**/*.{html,js}",
-    "./gen-23.07-tailwind-css/src/backpack.html",
-    "./gen-23.07-tailwind-css/src/bike-light.html"
+    "./gen-23.07-tailwind-css/src/**/*.{html,js}"
   ],
   theme: {
     extend: {},
