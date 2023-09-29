df5c1620adf1ac8d0930baeee41f3c5121a715a4
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-09-29 13:42:40 +0700

    add link href to all product details

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./gen-23.07-tailwind-css/src/**/*.{html,js}",
+    "./gen-23.07-tailwind-css/src/backpack.html",
+    "./gen-23.07-tailwind-css/src/bike-light.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
