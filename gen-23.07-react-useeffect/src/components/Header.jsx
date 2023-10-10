b1c0c9ec09cd74bc351218ea489c8158f91daa6b
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-10 21:18:26 +0700

    copy project

---
+++ gen-23.07-react-useeffect/src/components/Header.jsx
@@ -0,0 +1,56 @@
+function Header() {
+  return (
+    <div>
+      <div className="flex items-center justify-between p-3 border-b-2 border-gray-100">
+        {/* Menu Burger Icon*/}
+        <div className="h-7 w-7">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+
+        <h1 className="text-2xl">Swag Labs</h1>
+
+        {/* Cart Icon*/}
+        <div className="h-7 w-7">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+          </svg>
+        </div>
+      </div>
+
+      <div className="p-4 border-b-2 border-gray-100">
+        <div className="flex justify-between">
+          <div className="ml-1 text-xl font-semibold">Products</div>
+
+          <div>
+            <select className="border-2 border-gray-200 rounded-sm" name="filter">
+              <option value="name-asc">Name (A to Z)</option>
+              <option value="name-des">Name (Z to A)</option>
+              <option value="price-asc">Price (low to high)</option>
+              <option value="name-des">Price (high to low)</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
