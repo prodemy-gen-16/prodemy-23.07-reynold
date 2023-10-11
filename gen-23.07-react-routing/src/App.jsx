0518d6e7bfb9efc9aef3a3fc711ff4bd6f745225
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-12 00:06:08 +0700

    add routes

--- gen-23.07-react-routing/src/App.jsx
+++ gen-23.07-react-routing/src/App.jsx
@@ -1,11 +1,19 @@
 import "./index.css";
 import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <ProductList />
-    </>
+    // <>
+    //   <ProductList />
+    // </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
