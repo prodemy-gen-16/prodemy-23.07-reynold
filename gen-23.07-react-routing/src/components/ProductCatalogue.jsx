d590be771a88f8e1a07628d10dc52fda585772e3
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-12 11:30:20 +0700

    use select option instead button for sorting products

--- gen-23.07-react-routing/src/components/ProductCatalogue.jsx
+++ gen-23.07-react-routing/src/components/ProductCatalogue.jsx
@@ -4,19 +4,24 @@ import productData from "../data/ProductData.json";
 
 function ProductCatalogue() {
   const navigate = useNavigate();
-  
+
   const initialData = productData.products;
 
   const [data, setData] = useState(initialData);
   const [selectedSortOption, setSelectedSortOption] = useState();
 
-
-  // Set Products Sorting by Recent Date by Default
+  // Set Products Sorting by Latest Date by Default
   useEffect(() => {
-    handleSortProducts("recent");
+    sortProducts("latest");
   }, []);
 
-  const handleSortProducts = (sortType) => {
+  const handleSortChange = (event) => {
+    const sortType = event.target.value;
+    setSelectedSortOption(sortType);
+    sortProducts(sortType);
+  };
+
+  const sortProducts = (sortType) => {
     // Change Button Color When Selected
     setSelectedSortOption(sortType);
 
@@ -25,8 +30,8 @@ function ProductCatalogue() {
         return parseFloat(a.price) - parseFloat(b.price); // Sort Products by Lowest Price
       } else if (sortType === "highToLow") {
         return parseFloat(b.price) - parseFloat(a.price); // Sort Products by Highest Price
-      } else if (sortType === "recent") {
-        return new Date(b.releaseDate) - new Date(a.releaseDate); // Sort Products by Recent Date
+      } else if (sortType === "latest") {
+        return new Date(b.releaseDate) - new Date(a.releaseDate); // Sort Products by Latest Date
       }
     });
 
@@ -34,7 +39,7 @@ function ProductCatalogue() {
   };
 
   const handleGoToDetail = (id) => {
-    navigate(`/products/${id}`)
+    navigate(`/products/${id}`);
   };
 
   return (
@@ -44,69 +49,20 @@ function ProductCatalogue() {
         <div className="flex justify-between">
           <div className="ml-1 text-xl font-semibold">Products</div>
 
+          {/* Sort Options */}
           <div>
             <select
               className="border-2 border-gray-200 rounded-sm"
               name="filter"
+              value={selectedSortOption}
+              onChange={handleSortChange}
             >
-              <option value="name-asc">Name (A to Z)</option>
-              <option value="name-des">Name (Z to A)</option>
-              <option value="price-asc">Price (low to high)</option>
-              <option value="name-des">Price (high to low)</option>
+              <option value="latest">Latest</option>
+              <option value="lowToHigh">Low to High</option>
+              <option value="highToLow">High to Low</option>
             </select>
           </div>
         </div>
-      </div>
-
-      {/* Filter/Sort */}
-      <div className="w-3/4 h-8 mx-auto flex gap-x-5 my-5">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
-          />
-        </svg>
-
-        <button
-          className={`w-auto px-5 border-x border-y border-emerald-950 rounded-md ${
-            selectedSortOption === "lowToHigh"
-              ? "bg-emerald-950 text-white"
-              : "hover:bg-emerald-950 hover:text-white"
-          }`}
-          onClick={() => handleSortProducts("lowToHigh")}
-        >
-          Low to High
-        </button>
-
-        <button
-          className={`w-auto px-5 border-x border-y border-emerald-950 rounded-md ${
-            selectedSortOption === "highToLow"
-              ? "bg-emerald-950 text-white"
-              : "hover:bg-emerald-950 hover:text-white"
-          }`}
-          onClick={() => handleSortProducts("highToLow")}
-        >
-          High to Low
-        </button>
-
-        <button
-          className={`w-auto px-5 border-x border-y border-emerald-950 rounded-md ${
-            selectedSortOption === "recent"
-              ? "bg-emerald-950 text-white"
-              : "hover:bg-emerald-950 hover:text-white"
-          }`}
-          onClick={() => handleSortProducts("recent")}
-        >
-          Latest
-        </button>
       </div>
 
       {/* Products List */}
@@ -118,7 +74,7 @@ function ProductCatalogue() {
           >
             {/* Image */}
             <img
-              className="lg:w-[160px] object-cover sm:w-2/5 sm:rounded-tl-lg sm:rounded-bl-lg"
+              className="lg:max-w-[160px] lg:min-h-[200px] object-cover sm:w-2/5 sm:rounded-tl-lg sm:rounded-bl-lg"
               src={item.image[0]}
               alt="Products"
             />
