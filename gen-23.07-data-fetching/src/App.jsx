0f3ceaac1daa48e2749c8f3d24dcc5d9ed0cdb3c
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-15 19:59:10 +0700

    copy project

---
+++ gen-23.07-data-fetching/src/App.jsx
@@ -0,0 +1,18 @@
+import "./index.css";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" />}/>
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ gen-23.07-data-fetching/src/pages/ProductList.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Products from "../components/ProductCatalogue";
+import Footer from "../components/Footer";
+
+function ProductList() {
+  return (
+    <>
+      <Header />
+      <Products />
+      <Footer />
+    </>
+  );
+}
+
+export default ProductList
