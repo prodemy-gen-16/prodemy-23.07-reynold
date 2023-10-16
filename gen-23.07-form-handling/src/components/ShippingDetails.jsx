d65632cf3431a381391419d3d68e152cd2e3a1f8
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-16 20:44:32 +0700

    add second header

---
+++ gen-23.07-form-handling/src/components/ShippingDetails.jsx
@@ -0,0 +1,14 @@
+function ShippingDetails() {
+  return (
+    <div className="container min-w-full mx-0">
+      {/* Second Header */}
+      <div className="flex items-center p-4 border-b-2 border-gray-100">
+        <div className="ml-1 text-xl font-semibold">
+          Checkout: Your Information
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShippingDetails;
--- gen-23.07-form-handling/src/pages/Checkout.jsx
+++ gen-23.07-form-handling/src/pages/Checkout.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import ShippingDetails from '../components/ShippingDetails';
 import Footer from "../components/Footer";
 
 function ProductCatalogue() {
   return (
     <>
       <Header />
+      <ShippingDetails />
       <Footer />
     </>
   );
