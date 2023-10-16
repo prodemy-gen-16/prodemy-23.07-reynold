1f319f99f6b36ae1b85df3e62b1a5a1eaf76555f
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-16 20:34:18 +0700

    add checkout route

--- gen-23.07-form-handling/src/App.jsx
+++ gen-23.07-form-handling/src/App.jsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import ProductDetail from "./pages/ProductDetail";
 import ProductCatalogue from "./pages/ProductCatalogue";
+import Checkout from './pages/Checkout';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/products" element={<ProductCatalogue />} />
         <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ gen-23.07-form-handling/src/pages/Checkout.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function ProductCatalogue() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default ProductCatalogue
