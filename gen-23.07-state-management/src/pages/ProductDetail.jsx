a32e5fd8120f37cd210a55ecb288c1d149dc2ee2
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-18 14:08:12 +0700

    copy project

---
+++ gen-23.07-state-management/src/pages/ProductDetail.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import ProductCard from "../components/ProductCard";
+import Footer from "../components/Footer";
+
+function ProductDetail() {
+  return (
+    <>
+      <Header />
+      <ProductCard />
+      <Footer />
+    </>
+  );
+}
+
+export default ProductDetail;
