6d9062cf8a4392dc405bce703dec13b233f5e156
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-22 22:11:22 +0700

    styling cart item counter

--- gen-23.07-redux-toolkit/src/components/Header.jsx
+++ gen-23.07-redux-toolkit/src/components/Header.jsx
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Header() {
-  const { list } = useSelector((state) => state.cart);
   const dataCheckout = useSelector((state) => state.cart);
 
   return (
@@ -31,17 +30,25 @@ function Header() {
         </Link>
 
         {/* Cart Icon*/}
-        <Link to={"/checkout-information"} className="h-7 w-7">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-          </svg>
-          <span>{dataCheckout.totalQuantity}</span>
-        </Link>
+        <div>
+          <Link to={"/checkout-information"} className="h-7 w-7">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+            </svg>
+            {dataCheckout.totalQuantity > 0 ? (
+              <span className="absolute top-2 right-2 w-[18px] h-[18px] bg-green-500 text-white text-xs rounded-full flex items-center justify-center">
+                {dataCheckout.totalQuantity}
+              </span>
+            ) : (
+              <div></div>
+            )}
+          </Link>
+        </div>
       </div>
     </>
   );
